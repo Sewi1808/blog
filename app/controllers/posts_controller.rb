da2e45daecd3914361fd18bdffcfbf21f6edf2e5
Sewi1808 <szalasny.s@gmail.com>
2018-09-09 18:15:15 +0200

    create post CRUD ability

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,51 @@
 class PostsController < ApplicationController
-    def index
+  before_action :find_post, only: [:show, :edit, :destroy]
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render "new"
     end
+  end
 
-    def new
-      @post = Post.new
+  def edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render "edit"
     end
+  end
 
-    def Create
-      @post = Post.new(post_params)
-      if post.save
-        redirect_to @post
-      else
-        render "new"
-    end
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
 
-    private
+  private
 
-    def post_params
-      params.require(:post).permit(:title, :body)
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
 end
